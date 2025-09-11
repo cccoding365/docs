b6698ef338b99d32940c95ed86d3b5324acacba5
DoubledConG <992196871@qq.com>
2025-09-11 23:09:32 +0800

    chore: 调整配置

--- docs/.vitepress/config/index.mjs
+++ docs/.vitepress/config/index.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vitepress';
-import { localSearchOptions, themeConfig } from './zh-Hans';
 
 export default defineConfig({
     lang: 'zh-CN',
@@ -7,12 +6,47 @@ export default defineConfig({
     description: "一个记录程序员成长与生活的随笔集，分享编程笔记、个人生活感悟以及对人生、社会的思考。",
     head: [['link', { rel: 'icon', href: '/favicon.svg' }]],
     themeConfig: {
-        ...themeConfig,
-        // logo: 'logo.svg',
+        logo: 'logo.svg',
+        docFooter: {
+            prev: '上一篇',
+            next: '下一篇'
+        },
+        outline: {
+            label: '页面导航'
+        },
+        lastUpdated: {
+            text: '最后更新于'
+        },
+        langMenuLabel: '多语言',
+        returnToTopLabel: '回到顶部',
+        sidebarMenuLabel: '菜单',
+        darkModeSwitchLabel: '主题',
+        lightModeSwitchTitle: '切换到浅色模式',
+        darkModeSwitchTitle: '切换到深色模式',
         search: {
             provider: 'local',
             options: {
-                ...localSearchOptions
+                translations: {
+                    button: {
+                        buttonText: '搜索文档',
+                        buttonAriaLabel: '搜索文档'
+                    },
+                    modal: {
+                        displayDetails: '显示详细列表',
+                        resetButtonTitle: '清除查询条件',
+                        backButtonTitle: '关闭搜索',
+                        noResultsText: '无法找到相关结果',
+                        footer: {
+                            selectText: '选择',
+                            selectKeyAriaLabel: '进行选择',
+                            navigateText: '切换',
+                            navigateUpKeyAriaLabel: '向上',
+                            navigateDownKeyAriaLabel: '向下',
+                            closeText: '关闭',
+                            closeKeyAriaLabel: 'esc'
+                        }
+                    }
+                }
             }
         },
         socialLinks: [
